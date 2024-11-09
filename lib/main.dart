36d1b90de615f37a24e855922cbf8ea6547e3609
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-11-09 18:12:15 +0200

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/movie_app.dart';
+
+void main() {
+  runApp(const MovieApp());
+}
+
+
---
+++ lib/movie_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
